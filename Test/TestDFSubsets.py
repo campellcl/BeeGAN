c6e2430ed5bfba046562898ed22e06c938f8c8c9
ccampell <ccampell.asu@gmail.com>
2021-01-15 16:29:10 -0500

    Updated test file with progress.

--- Test/TestDFSubsets.py
+++ Test/TestDFSubsets.py
@@ -22,12 +22,33 @@ random_num = np.random.choice(
 df = pd.DataFrame({'date': date, 'random_num': random_num})
 # Sort by date in ascending order:
 df = df.sort_values(by="date")
+df['year'] = df['date'].dt.year
+df['week'] = df['date'].dt.week
+df['day_of_year'] = df['date'].dt.day
+df['day_of_week'] = df['date'].dt.dayofweek
 
-for year in df['date'].dt.year.unique():
-    year_df_subset = df[df['date'].dt.year == year]
-    # Iterate by week over all the existing data in the year:
-    for week in year_df_subset['date'].dt.week.unique():
-        week_df_subset = year_df_subset[year_df_subset['date'].dt.week == week]
-        pass
+df['yr_week_grp_idx'] = df['date'].apply(
+    lambda x: '%s-%s' % (x.year, '{:02d}'.format(x.week)))
+
+
+def perform_weekly_train_val_test_split(week_data: pd.Series):
+    # Randomly select from day-of-week indices [0-6] inclusive, 4 days for train, 2 for val, 1 for test
+    day_of_week_indices = np.arange(0, 7)
+    day_of_week_indices = np.random.permutation(day_of_week_indices)
+    train_days = day_of_week_indices[0: 4]
+    val_days = day_of_week_indices[4: 6]
+    test_day = day_of_week_indices[-1]
+    train_meta_series: pd.Series = week_data[week_data['day_of_week'] in train_days]
+    # Return separate train, test, and val pd.Series for audio files belonging to the specified days of the week:
+    return week_data['date'].dt.dayofweek.count()
+
+df.groupby('yr_week_grp_idx').apply(perform_weekly_train_val_test_split)
+
+# for year in df['date'].dt.year.unique():
+#     year_df_subset = df[df['date'].dt.year == year]
+#     # Iterate by week over all the existing data in the year:
+#     for week in year_df_subset['date'].dt.week.unique():
+#         week_df_subset = year_df_subset[year_df_subset['date'].dt.week == week]
+#         pass
 
 print('break')
