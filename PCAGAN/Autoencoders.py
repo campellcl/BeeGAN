6c37838908ec26dd4dc993a4693aaaa0293a5008
ccampell <ccampell.asu@gmail.com>
2021-03-07 19:39:34 -0500

    Added additional comments and progress on the Autoencoders.py file. It looks like the TF + Keras integration for the Model.fit() method requires a (data, label) pair which will not work natively with unlabeled data.

--- PCAGAN/Autoencoders.py
+++ PCAGAN/Autoencoders.py
@@ -3,27 +3,59 @@ import tensorflow as tf
 from typing import Tuple, Optional
 from tensorflow.keras import initializers, constraints, regularizers, activations
 import argparse
+from tensorflow.python.keras.utils import losses_utils
 from Utils.EnumeratedTypes.DatasetSplitType import DatasetSplitType
 from Utils.TensorFlow.TFRecordLoader import TFRecordLoader
 
 
 class BottleneckLayer:
+    """
+    BottleneckLayer: A class storing the representation of the bottleneck layer/latent-encoding.
+    """
 
     def __init__(
-            self, num_neurons: int, activation: activations, kernel_initializer: initializers,
-            bias_initializer: initializers, kernel_regularizer: Optional[regularizers] = None,
-            bias_regularizer: Optional[regularizers] = None, activity_regularizer: Optional[regularizers] = None,
-            kernel_constraint: Optional[constraints] = None, bias_constraint: Optional[constraints] = None,
-            name: Optional[str] = 'bottleneck_layer'):
+            self, num_neurons: int, activation: activations, kernel_initializer: initializers.Initializer,
+            bias_initializer: initializers.Initializer, kernel_regularizer: Optional[regularizers.Regularizer] = None,
+            bias_regularizer: Optional[regularizers.Regularizer] = None,
+            activity_regularizer: Optional[regularizers.Regularizer] = None,
+            kernel_constraint: Optional[constraints.Constraint] = None,
+            bias_constraint: Optional[constraints.Constraint] = None, name: Optional[str] = 'bottleneck_layer'):
+        """
+        __init__: Initializer for objects of type BottleneckLayer.
+        :param num_neurons: <int> The number of units/neurons in the 1D bottleneck layer.
+        :param activation: <tf.keras.activations> The activation function pertaining to units/neurons in the bottleneck
+         layer (see: https://www.tensorflow.org/api_docs/python/tf/keras/activations).
+        :param kernel_initializer: <tf.keras.initializers.Initializer> The initialization technique for the weights in
+         the bottleneck layer of the network (see: https://www.tensorflow.org/api_docs/python/tf/keras/initializers).
+        :param bias_initializer: <tf.keras.initializers.Initializer> The initialization technique for the bias terms in
+         the bottleneck layer of the network (see: https://www.tensorflow.org/api_docs/python/tf/keras/initializers).
+        :param kernel_regularizer: <tf.keras.regularizers.Regularizer/None> The regularization technique for the weights
+         in the bottleneck layer of the network (see: https://www.tensorflow.org/api_docs/python/tf/keras/regularizers)
+         if any.
+        :param bias_regularizer: <tf.keras.regularizers.Regularizer/None> The regularization technique for the biases in
+         the bottleneck layer of the network (see: https://www.tensorflow.org/api_docs/python/tf/keras/regularizers) if
+         any.
+        :param activity_regularizer: <tf.keras.regularizers.Regularizer/None> The regularization technique for the
+         activation function results in the bottleneck layer of the network (see:
+         https://www.tensorflow.org/api_docs/python/tf/keras/regularizers) if any.
+        :param kernel_constraint: <tf.keras.constraints.Constraint/None> The function which should be used to impose
+         constraints on the weight values of the bottleneck layer in the neural network (if any). For more details, see:
+         https://www.tensorflow.org/api_docs/python/tf/keras/constraints.
+        :param bias_constraint: <tf.keras.constraints.Constraint/None> The function which should be used to impose
+         constraints on the bias values of the bottleneck layer in the neural network (if any). For more details, see:
+         https://www.tensorflow.org/api_docs/python/tf/keras/constraints.
+        :param name: <str> The name of the bottleneck layer in the neural network. This field is used as a key value
+         in the TensorFlow computational graph.
+        """
         self._num_neurons: int = num_neurons
         self._activation: activations = activation
-        self._kernel_initializer: initializers = kernel_initializer
-        self._bias_initializer: initializers = bias_initializer
-        self._kernel_regularizer: Optional[regularizers] = kernel_regularizer
-        self._bias_regularizer: Optional[regularizers] = bias_regularizer
-        self._activity_regularizer: Optional[regularizers] = activity_regularizer
-        self._kernel_constraint: Optional[constraints] = kernel_constraint
-        self._bias_constraint: Optional[constraints] = bias_constraint
+        self._kernel_initializer: initializers.Initializer = kernel_initializer
+        self._bias_initializer: initializers.Initializer = bias_initializer
+        self._kernel_regularizer: Optional[regularizers.Regularizer] = kernel_regularizer
+        self._bias_regularizer: Optional[regularizers.Regularizer] = bias_regularizer
+        self._activity_regularizer: Optional[regularizers.Regularizer] = activity_regularizer
+        self._kernel_constraint: Optional[constraints.Constraint] = kernel_constraint
+        self._bias_constraint: Optional[constraints.Constraint] = bias_constraint
         self._name: Optional[str] = name
         self._dense_layer = tf.keras.layers.Dense(
             units=self._num_neurons,
@@ -42,11 +74,19 @@ class BottleneckLayer:
         return self._dense_layer
 
 
-def make_autoencoder_model(receptive_field_shape: Tuple[int, Optional[int]], bottleneck_layer: BottleneckLayer, training_batch_size: int):
+def make_autoencoder_model(receptive_field_size: int, bottleneck_layer: BottleneckLayer, batch_size: int):
+    """
+    make_autoencoder_model: TODO: Docstrings.
+
+    :param receptive_field_size: <int> The number of units/neurons in the 1D input layer/receptive field.
+    :param bottleneck_layer:
+    :param batch_size:
+    :return:
+    """
     model = tf.keras.Sequential([
-        tf.keras.layers.InputLayer(input_shape=receptive_field_shape, batch_size=training_batch_size, name='encoder'),
+        tf.keras.layers.InputLayer(input_shape=receptive_field_size, batch_size=batch_size, name='encoder'),
         bottleneck_layer.dense_layer,
-        tf.keras.layers.Dense(units=receptive_field_shape, batch_size=training_batch_size, name='decoder')
+        tf.keras.layers.Dense(units=receptive_field_size, batch_size=batch_size, name='decoder')
     ])
     return model
 
@@ -83,6 +123,7 @@ def main(args):
         # https://www.tensorflow.org/guide/effective_tf2#use_tfconfigexperimental_run_functions_eagerly_when_debugging)
         tf.config.run_functions_eagerly(True)
 
+    # Obtain the TFRecord dataset corresponding to the requested dataset split ('train', 'val', 'test', 'all'):
     tf_record_loader: TFRecordLoader = TFRecordLoader(
         root_data_dir=root_data_dir,
         dataset_split_type=dataset_split_type,
@@ -102,20 +143,65 @@ def main(args):
     # There will be (dataset_batch_size x 4097 x 66) raw/encoded 2D spectrogram tensors in the tf_example_batch[1]:
     # spectrogram_tensor_batch = tf_example_batch[1]
 
+    # BottleneckLayer the encoding/latent-code of the data (here for the SVD case):
+    bottleneck_layer: BottleneckLayer = BottleneckLayer(
+        num_neurons=4097,
+        activation=tf.keras.activations.linear,
+        kernel_initializer=tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=0.05, seed=None),
+        bias_initializer=tf.keras.initializers.Zeros(),
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        kernel_constraint=None,
+        bias_constraint=None,
+        name='bottleneck_layer'
+    )
+
+    # Instantiate the model:
+    autoencoder = make_autoencoder_model(
+        receptive_field_size=4097,
+        bottleneck_layer=bottleneck_layer,
+        batch_size=batch_size
+    )
+    # Setup the compilation arguments for the model:
+    optimizer: tf.keras.optimizers.Optimizer = tf.keras.optimizers.SGD(
+        learning_rate=0.01,
+        momentum=0.0,
+        nesterov=True,
+        name='sgd_nesterov'
+    )
+    # see: https://www.tensorflow.org/api_docs/python/tf/keras/losses/BinaryCrossentropy#args_1
+    loss_func: tf.keras.losses.Loss = tf.keras.losses.BinaryCrossentropy(
+        from_logits=True,   # We are providing y_pred as the logits tensor, and not a stand-alone probability dist.
+        label_smoothing=0,
+        reduction=losses_utils.ReductionV2.AUTO,    # This is primarily relevant for distributed TensorFlow
+        name='binary_crossentropy'
+    )
+    # Compile the model (see: https://www.tensorflow.org/api_docs/python/tf/keras/Model#compile):
+    autoencoder.compile(
+        optimizer=optimizer,
+        loss=loss_func,
+        metrics=None,
+        loss_weights=None,
+        weighted_metrics=None,
+        run_eagerly=is_debug,
+        # The number of batches to run during each tf.function call. At most one full epoch per execution.
+        steps_per_execution=1
+    )
+    # Train/fit the model (see: https://www.tensorflow.org/api_docs/python/tf/keras/Model#fit):
+    autoencoder.fit(
+        x=???
+    )
     pass
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Autoencoder argument parser.')
     parser.add_argument('-v', '--verbose', action='store_true', dest='is_verbose', required=False)
-    parser.add_argument('--root-data-dir', type=str, action='store', dest='root_data_dir', required=True)
-    parser.add_argument('--dataset-split', type=str, action='store', dest='dataset_split_str', required=True,
+    parser.add_argument('--root-data-dir', type=str, nargs=1, action='store', dest='root_data_dir', required=True)
+    parser.add_argument('--dataset-split', type=str, nargs=1, action='store', dest='dataset_split_str', required=True,
                         help='The dataset split that should be loaded (e.g. train, test, val, or all).')
     parser.add_argument('--batch-size', type=int, action='store', dest='batch_size', required=True)
     parser.add_argument('--order-deterministically', type=bool, action='store', dest='order_deterministically', required=True)
-    # parser.add_argument('--train-batch-size', type=int, action='store', dest='train_batch_size', required=True,
-    #                     help='The size of the input batches for the neural network during training.')
-    # parser.add_argument('--val-batch-size', type=int, action='store', dest='val_batch_size', required=True,
-    #                     help='The size of the input batches for the neural network during training.')
     command_line_args = parser.parse_args()
     main(args=command_line_args)
