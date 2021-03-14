23b5fa9f55aaffeebe6c84b9c6768cf0b9fd08af
ccampell <ccampell.asu@gmail.com>
2021-03-14 17:41:23 -0400

    Modified TFRecordLoader.py to work with 1D sample instead of 2D spectrogram.

--- Utils/TensorFlow/TFRecordLoader.py
+++ Utils/TensorFlow/TFRecordLoader.py
@@ -64,7 +64,7 @@ class TFRecordLoader:
             compression_type='ZLIB',
             num_parallel_reads=tf.data.experimental.AUTOTUNE
         )
-        # De-serialize the DS of tf.train.Examples into tuple (tf.Tensor(tf.string), tf.Tensor(tf.float32 2D array)):
+        # De-serialize the DS of tf.train.Examples into tuple (tf.Tensor(tf.string), tf.Tensor(tf.float32 1D array)):
         dataset = dataset.map(lambda x: self.deserialize_tf_record(serialized_tf_record=x))
         # Pre-split the Dataset into batches for training:
         dataset = dataset.batch(batch_size=batch_size)
@@ -96,27 +96,28 @@ class TFRecordLoader:
         deserialize_tf_record: Takes in a serialized TFRecord file, and de-serializes the TFRecord into a tf.Tensor.
          Then this method will de-serialize the ByteList objects to return a tuple containing two Tensors:
             1. A tf.string Tensor containing the ISO 8601 datetime associated with the sample.
-            2. A tf.float32 2D Tensor containing the spectrogram of the audio sample.
+            2. A tf.float32 1D Tensor containing a single frequency sample of the spectrogram for a single audio sample.
         :param serialized_tf_record: <bytes> A raw TFRecord file which was previously created by serializing a
          tf.train.Example.
-        :returns iso_8601_tensor, spectrogram_tensor: <tuple<tf.Tensor, tf.Tensor>> A tuple containing a tf.string Tensor
-         which holds the ISO 8601 datetime representation associated with the audio file, and a tf.float32 tensor which
-         contains the 2D spectrogram associated with the original source audio file.
+        :returns iso_8601_tensor, frequencies_tensor: <tuple<tf.Tensor, tf.Tensor>> A tuple containing a tf.string
+         Tensor which holds the ISO 8601 datetime representation associated with the audio file, and a tf.float32 tensor
+          which contains the 1D spectrogram sample associated with the original source audio file.
         """
 
         '''
-        First we have to tell TensorFlow what the format of the encoded data was. As TensorFlow has no way of knowing from
-         the raw serialized byte representation the format of the original tf.train.Example before serialization to binary:
+        First we have to tell TensorFlow what the format of the encoded data was. As TensorFlow has no way of knowing 
+         from the raw serialized byte representation the format of the original tf.train.Example before serialization 
+         to binary:
         '''
         feature_description = {
-            'spectrogram': tf.io.FixedLenFeature([], tf.string),
-            # The (originally 2D source Tensor) is now a serialized ByteString
+            'frequencies': tf.io.FixedLenFeature([], tf.string),
+            # The (originally 1D source Tensor) is now a serialized ByteString
             'iso_8601': tf.io.FixedLenFeature([], tf.string)
         }
 
         ''' 
         Parse the single serialized tf.train.Example Tensor (made up of bytes) into its two serialized component Tensors 
-         (one for the ISO_8601 datetime string, and one for the 2D Tensor containing the spectrogram data):
+         (one for the ISO_8601 datetime string, and one for the 1D Tensor containing the sample frequency data):
         '''
         read_example: dict = tf.io.parse_example(
             serialized=serialized_tf_record,
@@ -139,16 +140,16 @@ class TFRecordLoader:
         # tf.strings.unicode_decode(iso_8601_tensor, input_encoding='UTF-8')
 
         '''
-        The Spectrogram was serialized as a list of bytes in order to store the 2D source Tensor in the TFRecord format. We
-         now need to convert that list of bytes back into a tf.Tensor object:
+        The Spectrogram was serialized as a list of bytes in order to store the 1D source Tensor in the TFRecord format.
+         We now need to convert that list of bytes back into a tf.Tensor object:
         '''
-        spectrogram_bytes_list_tensor: tf.Tensor = read_example['spectrogram']
-        spectrogram_tensor: tf.Tensor = tf.io.parse_tensor(
-            serialized=spectrogram_bytes_list_tensor,
+        frequencies_bytes_list_tensor: tf.Tensor = read_example['frequencies']
+        frequencies_tensor: tf.Tensor = tf.io.parse_tensor(
+            serialized=frequencies_bytes_list_tensor,
             out_type=tf.float32,
-            name='spectrogram'
+            name='frequencies'
         )
-        return iso_8601_tensor, spectrogram_tensor
+        return iso_8601_tensor, frequencies_tensor
 
 
 def main(args):
@@ -198,8 +199,12 @@ def main(args):
     # There will be (dataset_batch_size x 1) raw/encoded ISO 8601 tensors in the tf_example_batch[0]:
     iso_8601_tensor_batch = tf_example_batch[0]
 
-    # There will be (dataset_batch_size x 4097 x 66) raw/encoded 2D spectrogram tensors in the tf_example_batch[1]:
+    # There will be (dataset_batch_size x 4097) raw/encoded 2D spectrogram tensors in the tf_example_batch[1]:
     spectrogram_tensor_batch = tf_example_batch[1]
+
+    # A single sample by default size (4097,) is hence:
+    sample = spectrogram_tensor_batch[0]
+    sample_iso_8601_str = iso_8601_tensor_batch[0]
 
     exit(0)
 
